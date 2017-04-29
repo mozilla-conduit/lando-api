79772abcc0c9759ec4588466c89bb9a9b4423af8
Mars <mars@mozilla.com>
2017-04-28 21:33:33 -0400

    Add Dockerflow API endpoints
    Add API endpoints so we conform to Cloud Ops' Dockerflow spec.

--- landoapi/app.py
+++ landoapi/app.py
@@ -6,23 +6,34 @@ import os
 import click
 import connexion
 from connexion.resolver import RestyResolver
+from landoapi.dockerflow import dockerflow
 
 
-def create_app():
+def create_app(version_path):
+    """Construct an application instance."""
     app = connexion.App(__name__, specification_dir='spec/')
     app.add_api('swagger.yml', resolver=RestyResolver('landoapi.api'))
+
+    # Get the Flask app being wrapped by the Connexion app.
+    flask_app = app.app
+    flask_app.config['VERSION_PATH'] = version_path
+    flask_app.register_blueprint(dockerflow)
+
     return app
 
 
 @click.command()
 @click.option('--debug', envvar='DEBUG', is_flag=True)
 @click.option('--port', envvar='PORT', default=8888)
-def development_server(debug, port):
+@click.option(
+    '--version-path', envvar='VERSION_PATH', default='/app/version.json'
+)
+def development_server(debug, port, version_path):
     """Run the development server.
 
     This server should not be used for production deployments. Instead
     the application should be served by an external webserver as a wsgi
     app.
     """
-    app = create_app()
+    app = create_app(version_path)
     app.run(debug=debug, port=port, host='0.0.0.0')
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,15 +2,50 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
+
 import pytest
 from landoapi.app import create_app
 
 
 @pytest.fixture
-def app():
-    app = create_app()
+def versionfile(tmpdir):
+    """Provide a temporary version.json on disk."""
+    v = tmpdir.mkdir('app').join('version.json')
+    v.write(
+        json.dumps(
+            {
+                'source': 'https://github.com/mozilla-conduit/lando-api',
+                'version': '0.0.0',
+                'commit': '',
+                'build': 'test',
+            }
+        )
+    )
+    return v
+
+
+@pytest.fixture
+def app(versionfile):
+    """Needed for pytest-flask."""
+    app = create_app(versionfile.strpath)
     return app.app
 
 
 def test_app(client):
     assert client.get('/revisions/').status_code == 200
+
+
+def test_dockerflow_lb_endpoint_returns_200(client):
+    assert client.get('/__lbheartbeat__').status_code == 200
+
+
+def test_dockerflow_version_endpoint_response(client):
+    response = client.get('/__version__')
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+
+
+def test_dockerflow_version_matches_disk_contents(client, versionfile):
+    response = client.get('/__version__')
+    assert response.json == json.load(versionfile.open())
