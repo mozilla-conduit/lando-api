ce1a26d90046e53727f1619973405ce6b03a22f8
Mars <mars@mozilla.com>
2017-04-28 21:33:33 -0400

    Add Dockerflow API endpoints
    Add API endpoints so we conform to Cloud Ops' Dockerflow spec.

---
+++ landoapi/dockerflow.py
@@ -0,0 +1,46 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Endpoints to make this service comply with Cloud Ops' Dockerflow
+specification.  See https://github.com/mozilla-services/Dockerflow for details.
+"""
+
+import json
+
+from flask import Blueprint, current_app, jsonify
+
+dockerflow = Blueprint('dockerflow', __name__)
+
+
+@dockerflow.route('/__heartbeat__')
+def heartbeat():
+    """Perform an in-depth service health check.
+
+    This should check all the services that this service depends on
+    and return a 200 iff those services and the app itself are
+    performing normally. Return a 5XX if something goes wrong.
+    """
+    # TODO check backing services
+    return '', 200
+
+
+@dockerflow.route('/__lbheartbeat__')
+def lbheartbeat():
+    """Perform health check for load balancing.
+
+    Since this is for load balancer checks it should not check
+    backing services.
+    """
+    return '', 200
+
+
+@dockerflow.route('/__version__')
+def version():
+    """Respond with version information as defined by /app/version.json."""
+    try:
+        with open(current_app.config['VERSION_PATH']) as f:
+            return jsonify(json.load(f))
+    except (IOError, ValueError):
+        # TODO log error
+        return 'Unable to load version.json', 500
