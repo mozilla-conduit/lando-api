7c8a8c1005ffcee24322cb5881978810ca5d2579
Steven MacLeod <steven@smacleod.ca>
2019-09-06 14:49:48 -0400

    hgexports: port PatchHelper from transplant.

--- landoapi/hgexports.py
+++ landoapi/hgexports.py
@@ -1,9 +1,18 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-"""
-Module for constructing Mercurial patches in 'hg export' format.
-"""
+import re
+
+
+HEADER_NAMES = (
+    b"User",
+    b"Date",
+    b"Node ID",
+    b"Parent",
+    b"Diff Start Line",
+    b"Fail HG Import",
+)
+DIFF_LINE_RE = re.compile(rb"^diff\s+\S+\s+\S+")
 
 _HG_EXPORT_PATCH_TEMPLATE = """
 {header}
@@ -54,3 +63,120 @@ def build_patch_for_revision(
 def _no_line_breaks(s):
     """Return s with all line breaks removed."""
     return "".join(s.strip().splitlines())
+
+
+class PatchHelper(object):
+    """Helper class for parsing Mercurial patches/exports."""
+
+    def __init__(self, fileobj):
+        self.patch = fileobj
+        self.headers = {}
+        self.header_end_line_no = 0
+        self._parse_header()
+
+        # "Diff Start Line" is a Lando extension to the hg export
+        # format meant to prevent injection of diff hunks using the
+        # commit message.
+        self.diff_start_line = self.header(b"Diff Start Line")
+        if self.diff_start_line:
+            try:
+                self.diff_start_line = int(self.diff_start_line)
+            except ValueError:
+                self.diff_start_line = None
+
+    @staticmethod
+    def _is_diff_line(line):
+        return DIFF_LINE_RE.search(line)
+
+    @staticmethod
+    def _header_value(line, prefix):
+        m = re.search(
+            rb"^#\s+" + re.escape(prefix) + rb"\s+(.*)", line, flags=re.IGNORECASE
+        )
+        if not m:
+            return None
+        return m.group(1).strip()
+
+    def _parse_header(self):
+        """Extract header values specified by HEADER_NAMES."""
+        self.patch.seek(0)
+        try:
+            for line in self.patch:
+                if not line.startswith(b"# "):
+                    break
+                self.header_end_line_no += 1
+                for name in HEADER_NAMES:
+                    value = self._header_value(line, name)
+                    if value:
+                        self.headers[name.lower()] = value
+                        break
+        finally:
+            self.patch.seek(0)
+
+    def header(self, name):
+        """Returns value of the specified header, or None if missing."""
+        name = name.encode("utf-8") if isinstance(name, str) else name
+        return self.headers.get(name.lower())
+
+    def commit_description(self):
+        """Returns the commit description."""
+        try:
+            line_no = 0
+            commit_desc = []
+            for line in self.patch:
+                line_no += 1
+
+                if line_no <= self.header_end_line_no:
+                    continue
+
+                if self.diff_start_line:
+                    if line_no == self.diff_start_line:
+                        break
+                    commit_desc.append(line)
+                else:
+                    if self._is_diff_line(line):
+                        break
+                    commit_desc.append(line)
+
+            return b"".join(commit_desc).strip()
+        finally:
+            self.patch.seek(0)
+
+    def write(self, f):
+        """Writes whole patch to the specified file object."""
+        try:
+            while 1:
+                buf = self.patch.read(16 * 1024)
+                if not buf:
+                    break
+                f.write(buf)
+        finally:
+            self.patch.seek(0)
+
+    def write_commit_description(self, f):
+        """Writes the commit description to the specified file object."""
+        f.write(self.commit_description())
+
+    def write_diff(self, f):
+        """Writes the diff to the specified file object."""
+        try:
+            line_no = 0
+            for line in self.patch:
+                line_no += 1
+
+                if self.diff_start_line:
+                    if line_no == self.diff_start_line:
+                        f.write(line)
+                        break
+                else:
+                    if self._is_diff_line(line):
+                        f.write(line)
+                        break
+
+            while 1:
+                buf = self.patch.read(16 * 1024)
+                if not buf:
+                    break
+                f.write(buf)
+        finally:
+            self.patch.seek(0)
--- tests/test_hgexports.py
+++ tests/test_hgexports.py
@@ -1,8 +1,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import io
 
-from landoapi.hgexports import build_patch_for_revision
+from landoapi.hgexports import build_patch_for_revision, PatchHelper
+
+import pytest
 
 GIT_DIFF_FROM_REVISION = """diff --git a/hello.c b/hello.c
 --- a/hello.c   Fri Aug 26 01:21:28 2005 -0700
@@ -61,3 +64,218 @@ def test_build_patch():
     )
 
     assert patch == HG_PATCH
+
+
+@pytest.mark.parametrize(
+    "line, expected",
+    [
+        (b"diff --git a/file b/file", True),
+        (b"diff a/file b/file", True),
+        (b"diff -r 23280edf8655 autoland/autoland/patch_helper.py", True),
+        (b"cheese", False),
+        (b"diff", False),
+        (b"diff ", False),
+        (b"diff file", False),
+    ],
+)
+def test_patchhelper_is_diff_line(line, expected):
+    assert bool(PatchHelper._is_diff_line(line)) is expected
+
+
+def test_patchhelper_vanilla_export():
+    patch = PatchHelper(
+        io.BytesIO(
+            b"""
+# HG changeset patch
+# User byron jones <glob@mozilla.com>
+# Date 1523427125 -28800
+#      Wed Apr 11 14:12:05 2018 +0800
+# Node ID 3379ea3cea34ecebdcb2cf7fb9f7845861ea8f07
+# Parent  46c36c18528fe2cc780d5206ed80ae8e37d3545d
+WIP transplant and diff-start-line
+
+diff --git a/autoland/autoland/transplant.py b/autoland/autoland/transplant.py
+--- a/autoland/autoland/transplant.py
++++ b/autoland/autoland/transplant.py
+@@ -318,24 +318,58 @@ class PatchTransplant(Transplant):
+# instead of passing the url to 'hg import' to make
+...
+""".strip()
+        )
+    )
+    assert patch.header("Date") == b"1523427125 -28800"
+    assert patch.header("Node ID") == b"3379ea3cea34ecebdcb2cf7fb9f7845861ea8f07"
+    assert patch.header("User") == b"byron jones <glob@mozilla.com>"
+    assert patch.header("Parent") == b"46c36c18528fe2cc780d5206ed80ae8e37d3545d"
+    assert patch.commit_description() == b"WIP transplant and diff-start-line"
+
+
+def test_patchhelper_start_line():
+    patch = PatchHelper(
+        io.BytesIO(
+            b"""
+# HG changeset patch
+# User byron jones <glob@mozilla.com>
+# Date 1523427125 -28800
+#      Wed Apr 11 14:12:05 2018 +0800
+# Node ID 3379ea3cea34ecebdcb2cf7fb9f7845861ea8f07
+# Parent  46c36c18528fe2cc780d5206ed80ae8e37d3545d
+# Diff Start Line 10
+WIP transplant and diff-start-line
+
+diff --git a/autoland/autoland/transplant.py b/autoland/autoland/transplant.py
+--- a/autoland/autoland/transplant.py
++++ b/autoland/autoland/transplant.py
+@@ -318,24 +318,58 @@ class PatchTransplant(Transplant):
+# instead of passing the url to 'hg import' to make
+...
+""".strip()
+        )
+    )
+    assert patch.header("Diff Start Line") == b"10"
+    assert patch.commit_description() == b"WIP transplant and diff-start-line"
+
+
+def test_patchhelper_no_header():
+    patch = PatchHelper(
+        io.BytesIO(
+            b"""
+WIP transplant and diff-start-line
+
+diff --git a/autoland/autoland/transplant.py b/autoland/autoland/transplant.py
+--- a/autoland/autoland/transplant.py
++++ b/autoland/autoland/transplant.py
+@@ -318,24 +318,58 @@ class PatchTransplant(Transplant):
+# instead of passing the url to 'hg import' to make
+...
+""".strip()
+        )
+    )
+    assert patch.header("User") is None
+    assert patch.commit_description() == b"WIP transplant and diff-start-line"
+
+
+def test_patchhelper_diff_injection_no_start_line():
+    patch = PatchHelper(
+        io.BytesIO(
+            b"""
+# HG changeset patch
+# User byron jones <glob@mozilla.com>
+# Date 1523427125 -28800
+#      Wed Apr 11 14:12:05 2018 +0800
+# Node ID 3379ea3cea34ecebdcb2cf7fb9f7845861ea8f07
+# Parent  46c36c18528fe2cc780d5206ed80ae8e37d3545d
+WIP transplant and diff-start-line
+
+diff --git a/bad b/bad
+@@ -0,0 +0,0 @@
+blah
+
+diff --git a/autoland/autoland/transplant.py b/autoland/autoland/transplant.py
+--- a/autoland/autoland/transplant.py
++++ b/autoland/autoland/transplant.py
+@@ -318,24 +318,58 @@ class PatchTransplant(Transplant):
+# instead of passing the url to 'hg import' to make
+...
+""".strip()
+        )
+    )
+    assert patch.commit_description() == b"WIP transplant and diff-start-line"
+
+
+def test_patchhelper_diff_injection_start_line():
+    patch = PatchHelper(
+        io.BytesIO(
+            b"""
+# HG changeset patch
+# User byron jones <glob@mozilla.com>
+# Date 1523427125 -28800
+#      Wed Apr 11 14:12:05 2018 +0800
+# Node ID 3379ea3cea34ecebdcb2cf7fb9f7845861ea8f07
+# Parent  46c36c18528fe2cc780d5206ed80ae8e37d3545d
+# Diff Start Line 14
+WIP transplant and diff-start-line
+
+diff --git a/bad b/bad
+@@ -0,0 +0,0 @@
+blah
+
+diff --git a/autoland/autoland/transplant.py b/autoland/autoland/transplant.py
+--- a/autoland/autoland/transplant.py
++++ b/autoland/autoland/transplant.py
+@@ -318,24 +318,58 @@ class PatchTransplant(Transplant):
+# instead of passing the url to 'hg import' to make
+...
+""".strip()
+        )
+    )
+    assert patch.commit_description() == (
+        b"WIP transplant and diff-start-line\n"
+        b"\n"
+        b"diff --git a/bad b/bad\n"
+        b"@@ -0,0 +0,0 @@\n"
+        b"blah"
+    )
+
+
+def test_patchhelper_write_start_line():
+    header = b"""
+# HG changeset patch
+# User byron jones <glob@mozilla.com>
+# Date 1523427125 -28800
+#      Wed Apr 11 14:12:05 2018 +0800
+# Node ID 3379ea3cea34ecebdcb2cf7fb9f7845861ea8f07
+# Parent  46c36c18528fe2cc780d5206ed80ae8e37d3545d
+# Diff Start Line 10
+""".strip()
+    commit_desc = b"""
+WIP transplant and diff-start-line
+""".strip()
+    diff = b"""
+diff --git a/autoland/autoland/transplant.py b/autoland/autoland/transplant.py
+--- a/autoland/autoland/transplant.py
++++ b/autoland/autoland/transplant.py
+@@ -318,24 +318,58 @@ class PatchTransplant(Transplant):
+# instead of passing the url to 'hg import' to make
+...
+""".strip()
+    patch = PatchHelper(io.BytesIO(b"%s\n%s\n\n%s" % (header, commit_desc, diff)))
+
+    buf = io.BytesIO(b"")
+    patch.write_commit_description(buf)
+    assert buf.getvalue() == commit_desc
+
+    buf = io.BytesIO(b"")
+    patch.write_diff(buf)
+    assert buf.getvalue() == diff
+
+
+def test_patchhelper_write_no_start_line():
+    header = b"""
+# HG changeset patch
+# User byron jones <glob@mozilla.com>
+# Date 1523427125 -28800
+#      Wed Apr 11 14:12:05 2018 +0800
+# Node ID 3379ea3cea34ecebdcb2cf7fb9f7845861ea8f07
+# Parent  46c36c18528fe2cc780d5206ed80ae8e37d3545d
+""".strip()
+    commit_desc = b"""
+WIP transplant and diff-start-line
+""".strip()
+    diff = b"""
+diff --git a/autoland/autoland/transplant.py b/autoland/autoland/transplant.py
+--- a/autoland/autoland/transplant.py
++++ b/autoland/autoland/transplant.py
+@@ -318,24 +318,58 @@ class PatchTransplant(Transplant):
+# instead of passing the url to 'hg import' to make
+...
+""".strip()
+    patch = PatchHelper(io.BytesIO(b"%s\n%s\n\n%s" % (header, commit_desc, diff)))
+
+    buf = io.BytesIO(b"")
+    patch.write_commit_description(buf)
+    assert buf.getvalue() == commit_desc
+
+    buf = io.BytesIO(b"")
+    patch.write_diff(buf)
+    assert buf.getvalue() == diff
