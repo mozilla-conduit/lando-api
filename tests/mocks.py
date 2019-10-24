805319cd100bcc71d35565c0704dba7ae47a2f15
Mars <mars@mozilla.com>
2019-10-24 14:37:14 -0400

    tests: transactions can be searched by object name (#86)
    Transaction objects can now be searched for by both PHID and by
    object name, such as "D123".  This is necessary for situations where
    only the object name is locally available without a network
    request. It also matches the Phabricator API, which allows searches
    by both PHID and object name.

--- tests/mocks.py
+++ tests/mocks.py
@@ -1207,13 +1207,43 @@ class PhabricatorDouble:
 
         items = list(self._transactions)
 
-        if objectIdentifier:
-            items = [i for i in items if i["objectPHID"] == objectIdentifier]
-        else:
+        if not objectIdentifier:
             error_info = 'When calling "transaction.search", you must provide an object to retrieve transactions for.'  # noqa
             raise PhabricatorAPIException(
                 error_info, error_code="ERR-CONDUIT-CORE", error_info=error_info
             )
+
+        if not objectIdentifier.startswith("PHID-"):
+            # Assume the caller is searching by object name. Find the PHID of the
+            # named object the caller is searching for.
+            matches = [
+                obj for obj in self._phids if obj.get("name") == objectIdentifier
+            ]
+            if not matches:
+                error_info = f'No object "{objectIdentifier}" exists.'
+                raise PhabricatorAPIException(
+                    error_info, error_code="ERR-CONDUIT-CORE", error_info=error_info
+                )
+
+            objectIdentifier = matches[0]["phid"]
+
+        # Transactions are special. You can't retrieve them directly using search. I
+        # don't know why. You have to retrieve the transaction's parent object instead.
+        if objectIdentifier.startswith("PHID-XACT-"):
+            error_info = '[Invalid Translation!] Object "%s" does not implement "%s", so transactions can not be loaded for it.'  # noqa
+            raise PhabricatorAPIException(
+                error_info, error_code="ERR-CONDUIT-CORE", error_info=error_info
+            )
+
+        # Comments are special. You can't retrieve them directly using search. You
+        # have to retrieve the comment's parent object instead.
+        if objectIdentifier.startswith("PHID-XCMT-"):
+            error_info = f'No object "{objectIdentifier}" exists.'
+            raise PhabricatorAPIException(
+                error_info, error_code="ERR-CONDUIT-CORE", error_info=error_info
+            )
+
+        items = [i for i in items if i["objectPHID"] == objectIdentifier]
 
         if constraints and "phids" in constraints:
             phids = constraints["phids"]
