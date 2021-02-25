c7e4f116b852c79adc74aba66c42a03f8a67cf7f
Zeid Zabaneh <zeid@mozilla.com>
2021-02-25 16:20:06 -0500

    landing jobs: add stats endpoint (bug 1695047)
    Add an API endpoint to check average duration, counts, and number of
    attempts of landings given a date range.

--- landoapi/api/landing_jobs.py
+++ landoapi/api/landing_jobs.py
@@ -4,7 +4,9 @@
 import logging
 
 from connexion import ProblemException
+from datetime import datetime, timedelta
 from flask import g
+from sqlalchemy import func, Date
 
 from landoapi import auth
 from landoapi.models.landing_job import LandingJob, LandingJobStatus, LandingJobAction
@@ -70,3 +72,39 @@ def put(landing_job_id, data):
             f"Landing job status ({landing_job.status}) does not allow cancelling.",
             type="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400",
         )
+
+
+def get_stats(start_date: str = "", end_date: str = "") -> dict:
+    """Return landing job statistics between given dates.
+
+    Args:
+        start_date: A string representing the start date (e.g. 2020-01-23).
+        end_date: A string representing the end date.
+
+    Returns:
+        Some meta data and the aggregated statistics.
+    """
+    if start_date is None:
+        start_date = datetime.now().date().isoformat()
+    if end_date is None:
+        end_date = (datetime.now() + timedelta(days=1)).date().isoformat()
+
+    result = (
+        db.session.query(
+            func.avg(LandingJob.duration_seconds).label("duration_seconds__avg"),
+            func.avg(LandingJob.attempts).label("attempts__avg"),
+            func.sum(LandingJob.duration_seconds).label("duration_seconds__sum"),
+            func.count(LandingJob.id).label("id__count"),
+            LandingJob.created_at.cast(Date).label("day"),
+        )
+        .filter(
+            LandingJob.status == LandingJobStatus.LANDED,
+            LandingJob.created_at <= end_date,
+            LandingJob.created_at >= start_date,
+        )
+        .group_by("day")
+    )
+
+    return {
+        "data": [r._asdict() for r in result.all()],
+    }
