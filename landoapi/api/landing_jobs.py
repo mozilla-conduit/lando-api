274baa6102a79481afd90fba47b5d66f6c0fbf46
Zeid Zabaneh <zeid@mozilla.com>
2021-02-26 12:28:47 -0500

    code review feedback

--- landoapi/api/landing_jobs.py
+++ landoapi/api/landing_jobs.py
@@ -4,7 +4,7 @@
 import logging
 
 from connexion import ProblemException
-from datetime import datetime, timedelta
+from datetime import datetime
 from flask import g
 from sqlalchemy import func, Date
 
@@ -84,10 +84,26 @@ def get_stats(start_date: str = "", end_date: str = "") -> dict:
     Returns:
         Some meta data and the aggregated statistics.
     """
-    if start_date is None:
-        start_date = datetime.now().date().isoformat()
-    if end_date is None:
-        end_date = (datetime.now() + timedelta(days=1)).date().isoformat()
+    if not start_date:
+        start_date = datetime.now()
+    else:
+        start_date = datetime.fromisoformat(start_date)
+
+    if not end_date:
+        end_date = datetime.now()
+    else:
+        end_date = datetime.fromisoformat(end_date)
+
+    start_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0)
+    end_date = end_date.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+    if start_date > end_date:
+        raise ProblemException(
+            400,
+            "start_date must be on or before end_date.",
+            f"start_date provided: {start_date}, end_date provided: {end_date}.",
+            type="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400",
+        )
 
     result = (
         db.session.query(
