mozilla-conduit/lando-api

commit ec72f391543d34cd3ff8b26560f1ae088ae1517b

Bug 1745293: amend release managers review check to check all required uplift conditions (#180)
This commit extends the existing uplift blocking check to include
checking for a submitted uplift request form somewhere on the
stack. The function name is updated to reflect the change.

We also add code to add the commit message rewriting paths to
change `r=release-managers,relman-reviewer` to `a=relman-reviewer`
to conform with existing syntax in uplit repositories.

landoapi/api/stacks.py
@@ -17,6 +17,7 @@ from landoapi.projects import (
 )
 from landoapi.repos import get_repos_for_env
 from landoapi.reviews import (
+    approvals_for_commit_message,
     get_collated_reviewers,
     reviewers_for_commit_message,
     serialize_reviewers,
@@ -37,6 +38,7 @@ from landoapi.stacks import (
     request_extended_revision_data,
 )
 from landoapi.transplants import get_blocker_checks
+from landoapi.uplift import get_release_managers
 from landoapi.users import user_search
 from landoapi.validation import revision_id_to_int
 
@@ -79,7 +81,9 @@ def get(revision_id):
     landable_repos = get_landable_repos_for_revision_data(stack_data, supported_repos)
 
     other_checks = get_blocker_checks(
-        repositories=supported_repos, relman_group_phid=get_relman_group_phid(phab)
+        relman_group_phid=get_relman_group_phid(phab),
+        repositories=supported_repos,
+        stack_data=stack_data,
     )
 
     landable, blocked = calculate_landable_subgraphs(
@@ -100,6 +104,11 @@ def get(revision_id):
 
     secure_project_phid = get_secure_project_phid(phab)
     sec_approval_project_phid = get_sec_approval_project_phid(phab)
+    release_managers = get_release_managers(phab)
+    relman_phids = {
+        member["phid"]
+        for member in release_managers["attachments"]["members"]["members"]
+    }
 
     revisions_response = []
     for _phid, revision in stack_data.revisions.items():
@@ -118,10 +127,14 @@ def get(revision_id):
         accepted_reviewers = reviewers_for_commit_message(
             reviewers, users, projects, sec_approval_project_phid
         )
+        accepted_reviewers, approval_reviewers = approvals_for_commit_message(
+            reviewers, users, projects, relman_phids, accepted_reviewers
+        )
         commit_message_title, commit_message = format_commit_message(
             commit_description.title,
             bug_id,
             accepted_reviewers,
+            approval_reviewers,
             commit_description.summary,
             revision_url,
         )

landoapi/api/transplants.py
@@ -28,7 +28,11 @@ from landoapi.projects import (
     project_search,
 )
 from landoapi.repos import get_repos_for_env
-from landoapi.reviews import get_collated_reviewers, reviewers_for_commit_message
+from landoapi.reviews import (
+    approvals_for_commit_message,
+    get_collated_reviewers,
+    reviewers_for_commit_message,
+)
 from landoapi.revisions import (
     gather_involved_phids,
     get_bugzilla_bug,
@@ -51,6 +55,9 @@ from landoapi.transplants import (
     TransplantAssessment,
 )
 from landoapi.transplant_client import TransplantClient, TransplantError
+from landoapi.uplift import (
+    get_release_managers,
+)
 from landoapi.users import user_search
 from landoapi.validation import revision_id_to_int
 
@@ -156,7 +163,9 @@ def _assess_transplant_request(phab, landing_path):
     landable_repos = get_landable_repos_for_revision_data(stack_data, supported_repos)
 
     other_checks = get_blocker_checks(
-        repositories=supported_repos, relman_group_phid=get_relman_group_phid(phab)
+        repositories=supported_repos,
+        relman_group_phid=get_relman_group_phid(phab),
+        stack_data=stack_data,
     )
 
     landable, blocked = calculate_landable_subgraphs(
@@ -323,6 +332,11 @@ def post(data):
     ]
 
     sec_approval_project_phid = get_sec_approval_project_phid(phab)
+    release_managers = get_release_managers(phab)
+    relman_phids = {
+        member["phid"]
+        for member in release_managers["attachments"]["members"]["members"]
+    }
 
     # Build the patches to land.
     patch_urls = []
@@ -332,6 +346,11 @@ def post(data):
             reviewers, users, projects, sec_approval_project_phid
         )
 
+        # Find RelMan reviews for rewriting to `a=<reviewer>`.
+        accepted_reviewers, approval_reviewers = approvals_for_commit_message(
+            reviewers, users, projects, relman_phids, accepted_reviewers
+        )
+
         secure = revision_is_secure(revision, secure_project_phid)
         commit_description = find_title_and_summary_for_landing(phab, revision, secure)
 
@@ -339,6 +358,7 @@ def post(data):
             commit_description.title,
             get_bugzilla_bug(revision),
             accepted_reviewers,
+            approval_reviewers,
             commit_description.summary,
             urllib.parse.urljoin(
                 current_app.config["PHABRICATOR_URL"], "D{}".format(revision["id"])

landoapi/reviews.py
@@ -4,12 +4,15 @@
 
 import logging
 from collections import namedtuple
-from typing import List
+from typing import List, Tuple
 
 from landoapi.phabricator import (
     PhabricatorClient,
     PhabricatorCommunicationException,
     ReviewerStatus,
+)
+from landoapi.projects import (
+    RELMAN_PROJECT_SLUG,
 )
 
 logger = logging.getLogger(__name__)
@@ -165,3 +168,41 @@ def reviewers_for_commit_message(
         for phid, r in reviewers.items()
         if (phid != sec_approval_phid and r["status"] is ReviewerStatus.ACCEPTED)
     ]
+
+
+def approvals_for_commit_message(
+    reviewers: dict,
+    users: List[dict],
+    projects: List[dict],
+    relman_phids: List[dict],
+    accepted_reviewers: List[str],
+) -> Tuple[List[str], List[str]]:
+    """Turn a list of reviewer objects into a list of approval names.
+
+    The list holds release managers that approved the revision, to be re-written as
+    `a=<reviewer>` in the final commit message.
+
+    Args:
+        reviewers: Dict of {reviewer_phid: reviewer_data}
+        users: List of Phabricator Users that were involved in the revision.
+        projects: List of Phabricator Projects that were involved in the revision.
+        sec_approval_phid: The PHID string of the sec-approval project.
+
+    Returns:
+        A list of strings.
+    """
+    # Approvals are reviews where the user is in the `release-managers` group.
+    approvals = [
+        reviewer_identity(phid, users, projects).identifier
+        for phid, r in reviewers.items()
+        if phid in relman_phids and r["status"] == ReviewerStatus.ACCEPTED
+    ]
+
+    # Filter approvals from regular reviews.
+    accepted_reviewers = [
+        reviewer
+        for reviewer in accepted_reviewers
+        if reviewer not in approvals and reviewer != RELMAN_PROJECT_SLUG
+    ]
+
+    return accepted_reviewers, approvals

tests/test_revisions.py
@@ -10,9 +10,12 @@ from landoapi.repos import get_repos_for_env
 from landoapi.revisions import (
     check_author_planned_changes,
     check_diff_author_is_known,
-    check_relman_approval,
+    check_uplift_approval,
     revision_is_secure,
     revision_needs_testing_tag,
+)
+from landoapi.stacks import (
+    request_extended_revision_data,
 )
 
 pytestmark = pytest.mark.usefixtures("docker_env_vars")
@@ -55,7 +58,9 @@ def test_check_author_planned_changes_changes_planned(phabdouble):
     assert check_author_planned_changes(revision=revision) is not None
 
 
-def test_secure_api_flag_on_public_revision_is_false(db, client, phabdouble):
+def test_secure_api_flag_on_public_revision_is_false(
+    db, client, phabdouble, release_management_project
+):
     public_project = phabdouble.project("public")
     revision = phabdouble.revision(projects=[public_project])
 
@@ -67,7 +72,7 @@ def test_secure_api_flag_on_public_revision_is_false(db, client, phabdouble):
 
 
 def test_secure_api_flag_on_secure_revision_is_true(
-    db, client, phabdouble, secure_project
+    db, client, phabdouble, secure_project, release_management_project
 ):
     revision = phabdouble.revision(projects=[secure_project])
 
@@ -95,9 +100,8 @@ def test_secure_revision_is_secure(phabdouble, secure_project):
     assert revision_is_secure(revision, secure_project["phid"])
 
 
-def test_relman_approval_missing(phabdouble):
+def test_relman_approval_missing(phabdouble, release_management_project):
     """A repo with an approval required needs relman as reviewer"""
-    relman_group = phabdouble.project("release-managers")
     repo = phabdouble.repo(name="uplift-target")
     repos = get_repos_for_env("localdev")
     assert repos["uplift-target"].approval_required is True
@@ -108,24 +112,27 @@ def test_relman_approval_missing(phabdouble):
         attachments={"reviewers": True, "reviewers-extra": True, "projects": True},
     )
 
-    check = check_relman_approval(relman_group["phid"], repos)
-    assert (
-        check(revision=phab_revision, repo=phabdouble.api_object_for(repo))
-        == "The release-managers group was not requested for review"
+    phab_client = phabdouble.get_phabricator_client()
+    stack_data = request_extended_revision_data(phab_client, [revision["phid"]])
+
+    check = check_uplift_approval(release_management_project["phid"], repos, stack_data)
+    assert check(revision=phab_revision, repo=phabdouble.api_object_for(repo)) == (
+        "The release-managers group did not accept the stack: "
+        "you need to wait for a group approval from release-managers, "
+        "or request a new review."
     )
 
 
 @pytest.mark.parametrize("status", list(ReviewerStatus))
-def test_relman_approval_status(status, phabdouble):
+def test_relman_approval_status(status, phabdouble, release_management_project):
     """Check only an approval from relman allows landing"""
-    relman_group = phabdouble.project("release-managers")
     repo = phabdouble.repo(name="uplift-target")
     repos = get_repos_for_env("localdev")
     assert repos["uplift-target"].approval_required is True
 
     # Add relman as reviewer with specified status
-    revision = phabdouble.revision(repo=repo)
-    phabdouble.reviewer(revision, relman_group, status=status)
+    revision = phabdouble.revision(repo=repo, uplift="blah blah")
+    phabdouble.reviewer(revision, release_management_project, status=status)
 
     # Add a some extra reviewers
     for i in range(3):
@@ -136,14 +143,17 @@ def test_relman_approval_status(status, phabdouble):
         attachments={"reviewers": True, "reviewers-extra": True, "projects": True},
     )
 
-    check = check_relman_approval(relman_group["phid"], repos)
+    phab_client = phabdouble.get_phabricator_client()
+    stack_data = request_extended_revision_data(phab_client, [revision["phid"]])
+
+    check = check_uplift_approval(release_management_project["phid"], repos, stack_data)
     output = check(revision=phab_revision, repo=phabdouble.api_object_for(repo))
     if status == ReviewerStatus.ACCEPTED:
         assert output is None
     else:
         assert (
             output
-            == "The release-managers group did not accept that stack: you need to wait for a group approval from release-managers, or request a new review."  # noqa
+            == "The release-managers group did not accept the stack: you need to wait for a group approval from release-managers, or request a new review."  # noqa
         )
 
 

landoapi/transplants.py
@@ -17,9 +17,12 @@ from landoapi.reviews import calculate_review_extra_state, reviewer_identity
 from landoapi.revisions import (
     check_author_planned_changes,
     check_diff_author_is_known,
-    check_relman_approval,
+    check_uplift_approval,
     revision_is_secure,
     revision_needs_testing_tag,
+)
+from landoapi.stacks import (
+    RevisionData,
 )
 
 logger = logging.getLogger(__name__)
@@ -436,11 +439,13 @@ def check_landing_blockers(
     return TransplantAssessment()
 
 
-def get_blocker_checks(repositories: dict, relman_group_phid: str):
+def get_blocker_checks(
+    repositories: dict, relman_group_phid: str, stack_data: RevisionData
+):
     """Build all transplant blocker checks that need extra Phabricator data"""
     assert all(map(lambda r: isinstance(r, Repo), repositories.values()))
 
     return DEFAULT_OTHER_BLOCKER_CHECKS + [
-        # Configure relman check with extra data
-        check_relman_approval(relman_group_phid, repositories)
+        # Configure uplift check with extra data.
+        check_uplift_approval(relman_group_phid, repositories, stack_data)
     ]

landoapi/commit_message.py
@@ -69,10 +69,11 @@ def format_commit_message(
     title: str,
     bug: str,
     reviewers: List[str],
+    approvals: List[str],
     summary: str,
     revision_url: str,
     flags: List[str] = None,
-) -> Tuple[str]:
+) -> Tuple[str, str]:
     """
     Creates a default format commit message using revision metadata.
 
@@ -87,6 +88,7 @@ def format_commit_message(
         title: The first line of the original commit message.
         bug: The bug number to use or None.
         reviewers: A list of reviewer usernames.
+        approvals: A list of approval usernames.
         summary: A string containing the revision's summary.
         revision_url: The revision's url in Phabricator.
         flags: A list of flags to append to the title.
@@ -105,7 +107,7 @@ def format_commit_message(
 
     # Ensure that the actual reviewers are recorded in the
     # first line of the commit message.
-    title = replace_reviewers(title, reviewers)
+    title = replace_reviewers(title, reviewers, approvals)
 
     # Clear any leading / trailing whitespace.
     title = title.strip()
@@ -134,11 +136,15 @@ def parse_bugs(s):
     return [bug for bug in bugs if bug < 100000000]
 
 
-def replace_reviewers(commit_description, reviewers):
+def replace_reviewers(commit_description, reviewers, approvals):
     if not reviewers:
         reviewers_str = ""
     else:
         reviewers_str = "r=" + ",".join(reviewers)
+
+    if approvals:
+        reviewers_str += " a="
+        reviewers_str += ",".join(approvals)
 
     if commit_description == "":
         return reviewers_str

landoapi/phabricator.py
@@ -253,7 +253,7 @@ class PhabricatorClient:
 
         if len(result) > 1 or (not result and not none_when_empty):
             raise PhabricatorCommunicationException(
-                "Phabricator responded with unexpected data"
+                "Phabricator responded with unexpected data: %s" % result
             )
 
         return result[0] if result else None

landoapi/repos.py
@@ -96,6 +96,11 @@ class Repo:
         )
 
 
+SCM_ALLOW_DIRECT_PUSH = AccessGroup(
+    active_group="active_scm_allow_direct_push",
+    membership_group="all_scm_allow_direct_push",
+    display_name="Above Level 3 Commit Access",
+)
 SCM_LEVEL_3 = AccessGroup(
     active_group="active_scm_level_3",
     membership_group="all_scm_level_3",
@@ -176,6 +181,7 @@ REPO_CONFIG = {
             push_path="ssh://autoland.hg//repos/third-repo",
             pull_path="http://hg.test/third-repo",
             config_override={"fix.black:command": "black -- -"},
+            approval_required=True,
         ),
         # Approval is required for the uplift dev repo
         "uplift-target": Repo(
@@ -198,6 +204,7 @@ REPO_CONFIG = {
             access_group=SCM_CONDUIT,
             commit_flags=[DONTBUILD],
             config_override={"fix.black:command": "black -- -"},
+            approval_required=True,
         ),
         "vct": Repo(
             tree="vct",
@@ -280,6 +287,12 @@ REPO_CONFIG = {
             tree="mozilla-build",
             url="https://hg.mozilla.org/mozilla-build",
             access_group=SCM_LEVEL_3,
+        ),
+        "beta": Repo(
+            tree="mozilla-beta",
+            url="https://hg.mozilla.org/releases/mozilla-beta",
+            access_group=SCM_ALLOW_DIRECT_PUSH,
+            approval_required=True,
         ),
     },
 }

commit ae5dd931ba79ce059213e9417b4778c4a9dbc9eb

approvals: only rewrite RelMan reviews as `a=` for approval required repositories (Bug 1755932)
Fixes a regression caused by #180 where all RelMan reviews are
rewritten from `r=reviewer` to `a=reviewer`. This should only
occur on an uplift repository, so we amend usages of
`approvals_for_commit_message` to only apply when this flag is
set.

Some of the secure revision tests fail after this change due to
a missing repository tagged on the revision so we add a repo to
these tests.

landoapi/api/stacks.py
@@ -115,6 +115,7 @@ def get(revision_id):
         revision_phid = PhabricatorClient.expect(revision, "phid")
         fields = PhabricatorClient.expect(revision, "fields")
         diff_phid = PhabricatorClient.expect(fields, "diffPHID")
+        repo_phid = PhabricatorClient.expect(fields, "repositoryPHID")
         diff = stack_data.diffs[diff_phid]
         human_revision_id = "D{}".format(PhabricatorClient.expect(revision, "id"))
         revision_url = urllib.parse.urljoin(
@@ -127,9 +128,23 @@ def get(revision_id):
         accepted_reviewers = reviewers_for_commit_message(
             reviewers, users, projects, sec_approval_project_phid
         )
-        accepted_reviewers, approval_reviewers = approvals_for_commit_message(
-            reviewers, users, projects, relman_phids, accepted_reviewers
+
+        repo_short_name = PhabricatorClient.expect(
+            stack_data.repositories[repo_phid], "fields", "shortName"
         )
+        approval_required = (
+            repo_short_name in supported_repos
+            and supported_repos[repo_short_name].approval_required
+        )
+
+        # Only update the approvals/reviewers if `approval_required` is set on the repo.
+        if approval_required:
+            accepted_reviewers, approval_reviewers = approvals_for_commit_message(
+                reviewers, users, projects, relman_phids, accepted_reviewers
+            )
+        else:
+            approval_reviewers = []
+
         commit_message_title, commit_message = format_commit_message(
             commit_description.title,
             bug_id,
@@ -158,7 +173,7 @@ def get(revision_id):
                 "summary": commit_description.summary,
                 "commit_message_title": commit_message_title,
                 "commit_message": commit_message,
-                "repo_phid": PhabricatorClient.expect(fields, "repositoryPHID"),
+                "repo_phid": repo_phid,
                 "diff": serialize_diff(diff),
                 "author": author_response,
                 "reviewers": serialize_reviewers(reviewers, users, projects, diff_phid),

landoapi/api/transplants.py
@@ -347,9 +347,12 @@ def post(data):
         )
 
         # Find RelMan reviews for rewriting to `a=<reviewer>`.
-        accepted_reviewers, approval_reviewers = approvals_for_commit_message(
-            reviewers, users, projects, relman_phids, accepted_reviewers
-        )
+        if landing_repo.approval_required:
+            accepted_reviewers, approval_reviewers = approvals_for_commit_message(
+                reviewers, users, projects, relman_phids, accepted_reviewers
+            )
+        else:
+            approval_reviewers = []
 
         secure = revision_is_secure(revision, secure_project_phid)
         commit_description = find_title_and_summary_for_landing(phab, revision, secure)

landoapi/reviews.py
@@ -186,10 +186,12 @@ def approvals_for_commit_message(
         reviewers: Dict of {reviewer_phid: reviewer_data}
         users: List of Phabricator Users that were involved in the revision.
         projects: List of Phabricator Projects that were involved in the revision.
+        relman_phids: List of Phabricator users in the `release-managers` group.
         sec_approval_phid: The PHID string of the sec-approval project.
 
     Returns:
-        A list of strings.
+        A tuple of lists of strings, representing `r=` reviewers and `a=` approvers
+        respectively.
     """
     # Approvals are reviews where the user is in the `release-managers` group.
     approvals = [

tests/test_revisions.py
@@ -61,8 +61,9 @@ def test_check_author_planned_changes_changes_planned(phabdouble):
 def test_secure_api_flag_on_public_revision_is_false(
     db, client, phabdouble, release_management_project
 ):
+    repo = phabdouble.repo(name="test-repo")
     public_project = phabdouble.project("public")
-    revision = phabdouble.revision(projects=[public_project])
+    revision = phabdouble.revision(projects=[public_project], repo=repo)
 
     response = client.get("/stacks/D{}".format(revision["id"]))
 
@@ -74,7 +75,8 @@ def test_secure_api_flag_on_public_revision_is_false(
 def test_secure_api_flag_on_secure_revision_is_true(
     db, client, phabdouble, secure_project, release_management_project
 ):
-    revision = phabdouble.revision(projects=[secure_project])
+    repo = phabdouble.repo(name="test-repo")
+    revision = phabdouble.revision(projects=[secure_project], repo=repo)
 
     response = client.get("/stacks/D{}".format(revision["id"]))
 