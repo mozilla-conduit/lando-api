c94d8179d66e531385405961d9e2a50cad26364b
Zeid Zabaneh <zeid@mozilla.com>
2022-04-07 15:18:39 -0400

    requirements: major package upgrades to Flask et al (bug 1754816)
    WIP DO NOT LAND
    
    - migrate to using sentry-sdk from raven
    - upgrade Flask, Connexion, Mercurial
    - remove old, stale requirements (blinker, py, pluggy, atomicwrites)
    
    TODO:
    - upgrade Celery

--- landoapi/sentry.py
+++ landoapi/sentry.py
@@ -3,12 +3,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import logging
 
-from raven.contrib.flask import Sentry
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from landoapi.systems import Subsystem
 
 logger = logging.getLogger(__name__)
-sentry = Sentry()
 
 
 class SentrySubsystem(Subsystem):
@@ -19,17 +19,11 @@ class SentrySubsystem(Subsystem):
 
         sentry_dsn = self.flask_app.config.get("SENTRY_DSN")
         logger.info("sentry status", extra={"enabled": bool(sentry_dsn)})
-        sentry.init_app(self.flask_app, dsn=sentry_dsn)
-
-        # Set these attributes directly because their keyword arguments can't be
-        # passed into Sentry.__init__() or make_client().
-        sentry.client.release = self.flask_app.config.get("VERSION").get(
-            "version", "0.0.0"
-        )
-        sentry.client.environment = self.flask_app.config.get("ENVIRONMENT")
-        sentry.client.processors = (
-            "raven.processors.SanitizePasswordsProcessor",
-            "raven.processors.RemoveStackLocalsProcessor",
+        sentry_sdk.init(
+            dsn=sentry_dsn,
+            integrations=[FlaskIntegration()],
+            traces_sample_rate=1.0,
+            release=self.flask_app.config.get("VERSION").get("version", "0.0.0"),
         )
 
 
