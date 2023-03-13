72c8655332055ee3ebaea7c7a0d026da9fdb3754
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2023-03-13 14:57:25 -0400

    stacks: refactor build_stack_graph endpoint and tests (bug 1819408) (#279)
    - use prefetched revision data to retrieve stack graph
    - parse edges and nodes from above data
    - update tests and fixtures

--- landoapi/stacks.py
+++ landoapi/stacks.py
@@ -25,18 +25,11 @@ from landoapi.phabricator import (
 logger = logging.getLogger(__name__)
 
 
-def build_stack_graph(
-    phab: PhabricatorClient, revision_phid: str
-) -> tuple[set[str], set[tuple[str, str]]]:
+def build_stack_graph(revision: dict) -> tuple[set[str], set[tuple[str, str]]]:
     """Return a graph representation of a revision stack.
 
-    This function is expensive and can make up to approximately
-    n/2 calls to phabricator for a linear stack where n is the
-    number of revisions in the stack.
-
     Args:
-        phab: A landoapi.phabricator.PhabricatorClient.
-        revision_phid: String PHID of a revision in the stack.
+        revision: A dictionary containing Phabricator revision data.
 
     Returns:
         A tuple of (nodes, edges). `nodes` is a set of strings
@@ -45,36 +38,13 @@ def build_stack_graph(
         between two nodes. `child` and `parent` are also string
         PHIDs.
     """
-    phids = set()
-    new_phids = {revision_phid}
-    edges = []
+    stack_graph = PhabricatorClient.expect(revision, "fields", "stackGraph")
+    phids = set(stack_graph.keys())
+    edges = set()
 
-    # Repeatedly request all related edges, adding connected revisions
-    # each time until no new revisions are found.
-    while new_phids:
-        phids.update(new_phids)
-        edges = phab.call_conduit(
-            "edge.search",
-            types=["revision.parent", "revision.child"],
-            sourcePHIDs=[phid for phid in phids],
-            limit=10000,
-        )
-        edges = phab.expect(edges, "data")
-        new_phids = set()
-        for edge in edges:
-            new_phids.add(edge["sourcePHID"])
-            new_phids.add(edge["destinationPHID"])
-
-        new_phids = new_phids - phids
-
-    # Treat the stack like a commit DAG, we only care about edges going
-    # from child to parent. This is enough to represent the graph.
-    edges = {
-        (edge["sourcePHID"], edge["destinationPHID"])
-        for edge in edges
-        if edge["edgeType"] == "revision.parent"
-    }
-
+    for node, predecessors in stack_graph.items():
+        for predecessor in predecessors:
+            edges.add((node, predecessor))
     return phids, edges
 
 
