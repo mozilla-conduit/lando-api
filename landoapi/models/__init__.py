5c9beeac9af2ceb81cc8d3043355efe57eef230e
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2023-05-18 12:21:22 -0400

    models: implement landing job revisions (bug 1825277) (#315)
    - create many to many relationship between revisions and landing jobs
     - create methods that populate landing jobs with revisions
     - update various legacy properties and fields accordingly
     - update all tests and fixtures to use new functionality
     - update warnings and checks to use new functionality
     - add some legacy handling for old-style landing jobs

--- landoapi/models/__init__.py
+++ landoapi/models/__init__.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from landoapi.models.landing_job import LandingJob
+from landoapi.models.revisions import Revision
 from landoapi.models.secapproval import SecApprovalRequest
 from landoapi.models.transplant import Transplant
 from landoapi.models.configuration import ConfigurationVariable
@@ -10,6 +11,7 @@ from landoapi.models.revisions import DiffWarning
 
 __all__ = [
     "LandingJob",
+    "Revision",
     "SecApprovalRequest",
     "Transplant",
     "ConfigurationVariable",
