9408730f4ac7cc57ddefc34acefcb4ed2a5baf6a
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2023-05-25 12:31:58 -0400

    Update landoapi/models/landing_job.py

--- landoapi/models/landing_job.py
+++ landoapi/models/landing_job.py
@@ -136,6 +136,7 @@ class LandingJob(Base):
 
     @property
     def landed_revisions(self):
+        """Return revision and diff ID mapping associated with the landing job."""
         revision_ids = [revision.id for revision in self.revisions]
         revision_to_diff_ids_query = (
             revision_landing_job.select()
