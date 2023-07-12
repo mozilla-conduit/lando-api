0a0155198d9f0bc56c816e0cba57f42e981273fa
Connor Sheehan <cosheehan@mozilla.com>
2023-07-12 12:11:52 -0400

    add git patch format test

--- tests/test_try.py
+++ tests/test_try.py
@@ -32,10 +32,31 @@ diff --git a/test.txt b/test.txt
 +adding another line
 """.strip()
 
+GIT_PATCH = rb"""From 0f5a3c99e12c1e9b0e81bed245fe537961f89e57 Mon Sep 17 00:00:00 2001
+From: Connor Sheehan <sheehan@mozilla.com>
+Date: Wed, 6 Jul 2022 16:36:09 -0400
+Subject: [PATCH] add another file
+
+add another file to the repo.
+---
+ landoui/errorhandlers.py | 8 +
+ 1 file changed, 1 insertions(+), 0 deletion(-)
+
+diff --git a/test.txt b/test.txt
+--- a/test.txt
++++ b/test.txt
+@@ -1,1 +1,2 @@
+ TEST
++adding another line
+--
+2.31.1
+""".strip()
+
 
 def test_try_api_requires_data(db, client, auth0_mock, mocked_repo_config):
     try_push_json = {
         "base_commit": "abc",
+        "patch_format": "hgexport",
         "patches": [],
     }
     response = client.post(
@@ -52,7 +73,7 @@ def test_try_api_requires_data(db, client, auth0_mock, mocked_repo_config):
     assert response.status_code == 400, "Try push without patches should return 400."
 
 
-def test_try_api_success(
+def test_try_api_success_hgexport(
     app,
     db,
     hg_server,
@@ -71,6 +92,65 @@ def test_try_api_success(
         "patch_format": "hgexport",
         "patches": [
             base64.b64encode(PATCH_WITHOUT_STARTLINE).decode("ascii"),
+        ],
+    }
+    response = client.post(
+        "/try/patches", json=try_push_json, headers=auth0_mock.mock_headers
+    )
+    assert response.status_code == 201, "Successful try push should return 201."
+    assert (
+        "id" in response.json
+    ), "Response should include the ID of the new landing job."
+
+    queue_items = LandingJob.job_queue_query(
+        repositories=["try"], grace_seconds=0
+    ).all()
+    assert len(queue_items) == 1, "Try push should have created 1 landing job."
+
+    # Run the landing job.
+    job = queue_items[0]
+
+    repo = Repo(
+        tree="try",
+        url=hg_server,
+        access_group=SCM_LEVEL_1,
+        push_path=hg_server,
+        pull_path=hg_server,
+        short_name="try",
+        phabricator_repo=False,
+        force_push=True,
+    )
+
+    worker = LandingWorker(sleep_seconds=0.01)
+    hgrepo = HgRepo(hg_clone.strpath)
+
+    assert worker.run_job(job, repo, hgrepo, treestatus)
+    assert job.status == LandingJobStatus.LANDED
+    assert len(job.landed_commit_id) == 40
+    assert (
+        job.target_commit_hash == "0da79df0ffff88e0ad6fa3e27508bcf5b2f2cec4"
+    ), "Target changeset should match the passed value."
+
+
+def test_try_api_success_gitformatpatch(
+    app,
+    db,
+    hg_server,
+    hg_clone,
+    treestatusdouble,
+    client,
+    auth0_mock,
+    mocked_repo_config,
+):
+    treestatus = treestatusdouble.get_treestatus_client()
+    treestatusdouble.open_tree("mozilla-central")
+
+    try_push_json = {
+        # The only node in the test repo.
+        "base_commit": "0da79df0ffff88e0ad6fa3e27508bcf5b2f2cec4",
+        "patch_format": "git-format-patch",
+        "patches": [
+            base64.b64encode(GIT_PATCH).decode("ascii"),
         ],
     }
     response = client.post(
