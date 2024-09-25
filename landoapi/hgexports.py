61ac78b9aaef3094ad7ff04cdcfcb0579b2c913a
Connor Sheehan <cosheehan@mozilla.com>
2024-09-25 13:34:19 -0400

    git-patch-helper: use `strip_git_version_info_lines` when empty commit is found (Bug 1921023) (#433)
    Previously we had just removed the last two lines of output,
    however if the patch has extra newlines this could fail and
    cause the patch to be deformed. Use the helper function we
    added to avoid issues with parsing the commit message.

--- landoapi/hgexports.py
+++ landoapi/hgexports.py
@@ -347,10 +347,12 @@ class GitPatchHelper(PatchHelper):
             commit_message_lines.append(line)
         else:
             # We never found the end of the commit message body, so this change
-            # must be an empty commit. Discard the last two lines of the
-            # constructed commit message which are Git version info and return
-            # an empty diff.
-            commit_message = "\n".join(commit_message_lines[:-2])
+            # must be an empty commit. Discard the Git version info from the commit
+            # message and return an empty diff.
+            commit_message_lines = GitPatchHelper.strip_git_version_info_lines(
+                commit_message_lines
+            )
+            commit_message = "\n".join(commit_message_lines)
             return commit_message, ""
 
         commit_message = "\n".join(commit_message_lines)
