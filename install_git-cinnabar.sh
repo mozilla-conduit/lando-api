95d47a9a50a11e7f7f55c16075d8f6daab928245
Connor Sheehan <cosheehan@mozilla.com>
2025-04-28 23:51:05 -0400

    use latest cinnabar

--- install_git-cinnabar.sh
+++ install_git-cinnabar.sh
@@ -2,6 +2,6 @@
 
 curl https://raw.githubusercontent.com/glandium/git-cinnabar/master/download.py -o download.py
 chmod u+x download.py
-./download.py --exact 0.7.0beta2
+./download.py --exact 0.7.2
 chmod a+x git-cinnabar
 chmod a+x git-remote-hg
